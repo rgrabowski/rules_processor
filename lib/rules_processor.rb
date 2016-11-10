5efb5906c2721fa3f19508020d186b2ecceb7c16
Rafal Grabowski <rafal.grabowski@gmail.com>
2016-11-10 10:36:15 +0100

    Initial commit

---
+++ lib/rules_processor.rb
@@ -0,0 +1,5 @@
+require "rules_processor/version"
+
+module RulesProcessor
+  # Your code goes here...
+end
---
+++ spec/rules_processor_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe RulesProcessor do
+  it "has a version number" do
+    expect(RulesProcessor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
